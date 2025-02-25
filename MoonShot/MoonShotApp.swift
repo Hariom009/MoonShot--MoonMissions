c7087a3927cd76c3d9780110ecde8489f1148141
Hariom009 <hariomconnects03@gmail.com>
2025-02-25 13:37:04 +0530

    Initial Commit

---
+++ MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Hari's Mac on 25.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoonShotTests/MoonShotTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotTests.swift
+//  MoonShotTests
+//
+//  Created by Hari's Mac on 25.02.2025.
+//
+
+import Testing
+@testable import MoonShot
+
+struct MoonShotTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
